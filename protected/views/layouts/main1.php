daa7dcfbd8d2366ffb7eb8de95794acb04bc2b43
manturakabchi <manturakabchi@gmail.com>
2014-09-09 04:29:01 +0000

    Cambios en el CSS

--- protected/views/layouts/main1.php
+++ protected/views/layouts/main1.php
@@ -30,145 +30,156 @@
 </head>
 
 <body>
+
 <div id="wrap">
-<div style="background-color:black; margin-bottom: 25px">mk</div>
+
+    <div class="barra_superior">
 
     <div class="container">
 
-<div class="row">
-
-                <div class="col-md-2">
-              <div class="bs-docs-sidebar hidden-xs hidden-sm affix" role="complementary">
-                <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logonuevo.png" width="130px" ><br><br>
-
-              </div>
-            </div>
-
-
-    <div class= "col-md-10">                
-    <nav class="navbar" role="navigation">
-
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                
-                <button type="button" class="navbar-default navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    
-                <span class="glyphicon glyphicon-chevron-down"></span>
-
-
-                </button>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav menuppal">
-                    <li>
-                        <a class="menuppal" href="#">Exposiciones | Ferias</a>
-                    </li>
-                    <li>
-                        <a class="menuppal" href="#">Artistas</a>
-                    </li>
-                    <li>
-                        <a class="menuppal" href="#">Alianzas</a>
-                    </li>
-                    <li>
-                        <a class="menuppal" href="#">Quienes somos</a>
-                    </li>
-                    <li>
-                        <a class="menuppal" href="#">Contacto</a>
-                    </li>
-
-                </ul>
-                
-            </div>
-            
-            <!-- /.navbar-collapse -->
-        <!-- /.container -->
-    </nav>
-
-
-        <!-- Portfolio Item Row -->
         <div class="row">
 
-            <div class="col-md-12">
+            <div class="col-md-2">
+                <img src="images/logoodalys.png" width="80px;">
+            </div>
+        </div>
+    </div>
 
-					<?php echo $content; ?>
-					
+
+    </div>
+
+    <div class="container">
+
+        <div class="row">
+
+            <div class="col-md-2">
+                <div class="bs-docs-sidebar hidden-xs hidden-sm affix" role="complementary">
+                    <img src="images/logonuevo.png" width="130px" ><br><br>
                 </div>
+            </div>
+
+            <div class= "col-md-10">     
+                <div class="visible-xs-block visible-sm-block">
+                    <img src="images/logonuevo.png" width="130px" ><br><br>
+                </div>
+                   
+                <nav class="navbar" role="navigation">
+
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            
+                            <button type="button" class="navbar-default navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                
+                            <span class="glyphicon glyphicon-chevron-down"></span>
+                            </button>
+
+                        </div>
+
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav menuppal">
+                                <li>
+                                    <a class="menuppal" href="#">Exposiciones | Ferias</a>
+                                </li>
+                                <li>
+                                    <a class="menuppal" href="#">Artistas</a>
+                                </li>
+                                <li>
+                                    <a class="menuppal" href="#">Alianzas</a>
+                                </li>
+                                <li>
+                                    <a class="menuppal" href="#">Quienes somos</a>
+                                </li>
+                                <li>
+                                    <a class="menuppal" href="#">Contacto</a>
+                                </li>
+
+                            </ul>
+                            
+                        </div>
+                        
+                        <!-- /.navbar-collapse -->
+                    <!-- /.container -->
+                </nav>
+                <div class="visible-xs-block visible-sm-block">
+                    <br><br>
+                </div>
+                   
+                <!-- Portfolio Item Row -->
+                <div class="row">
+
+                    <div class="col-md-12">
+
+					   <?php echo $content; ?>
+					   
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="footer">
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>Sobre Elemental</h5>
+                <p style="font-size:12px;">ELEMENTAL se aleja de la clásica relación galería-comisario-artista para plantear la construcción de las exposiciónes de una manera orgánica.
+                <a href="sobreElemental.php"><w style="font-size: 11px;"> (Ver más) </w></a></p>
+                <br>
+                <img src="images/logoodalys.png" width="120px;">
 
             </div>
 
+            <div class="col-md-2">
+                <h5>Navega</h5>
+                <p style="font-size:12px;">
+                    Exposiciones | Ferias <br>
+                    Artistas<br>
+                    Alianzas<br>
+                    Quienes somos<br>
+                    Contacto
+                </p>
 
+            </div>
 
+            <div class="col-md-2">
+                <h5>Redes Sociales</h5>
+            <p style="font-size:12px;line-height:30px;">
+
+                    <img src="images/social/Facebook-icon.png" width="25px"> Facebook<br>
+                    <img src="images/social/Twitter-icon.png" width="25px"> Twitter<br>
+                    <img src="images/social/Youtube-icon.png" width="25px"> Youtube<br>
+                    <img src="images/social/Instagram-icon.png" width="25px"> Instagram<br>
+                </p>
+            </div>
+
+            <div class="col-md-4">
+                <h5>Alianzas</h5>
+                            <p style="font-size:12px;line-height:40px;">
+
+                <img src="images/logos_alianzas/GI.png" width="80px"><br>
+                <img src="images/logos_alianzas/logo_berlin.png" height="36px;">
+
+                <img src="images/logos_alianzas/mome.png" height="22px;">
+
+                </p>
+            </div>
+        </div>
+        <hr class="clara">
+        <div class="row">
+            <div class="col-md-12">   
+                <p class="copyright">Copyright &copy; Your Website 2014</p>
+            </div>
         </div>
     </div>
-
-</div>
-
-</div>
-               <!-- /.row -->
-
-</div>
-<div id="footer">
-
-<!-- Footer -->
-<footer>
-    <div class="container">
-    <div class="row">
-        <div class="col-md-4">
-            <h5>Sobre Elemental</h5>
-           <p style="font-size:12px;">ELEMENTAL se aleja de la clásica relación galería-comisario-artista para plantear la construcción de las exposiciónes de una manera orgánica.
-            <a href="sobreElemental.php"><w style="font-size: 11px;"> (Ver más) </w></a></p>
-            <br>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logoodalys.png" width="120px;">
-
-        </div>
-
-        <div class="col-md-2">
-            <h5>Navega</h5>
-            <p style="font-size:12px;">
-            Exposiciones | Ferias <br>
-            Artistas<br>
-            Alianzas<br>
-            Quienes somos<br>
-            Contacto
-        </p>
-
-        </div>
-
-        <div class="col-md-2">
-            <h5>Redes Sociales</h5>
-            <p style="font-size:12px;">
-
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/social/Facebook-icon.png" width="25px"> Facebook<br>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/social/Twitter-icon.png" width="25px"> Twitter<br>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/social/Youtube-icon.png" width="25px"> Youtube<br>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/social/Instagram-icon.png" width="25px"> Instagram<br>
-        </p>
-
-        </div>
-
-        <div class="col-md-4">
-            <h5>Alianzas</h5>
-            <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/logos2.png" width="300px">
-
-
-        </div>
-        
-
-    </div>
-    <hr>
-     <div class="row">
-        <div class="col-md-12">   
-            <p>Copyright &copy; Your Website 2014</p>
-        </div>
-    <!-- /.row -->
-</div>
 </footer>
 </div>
-              
-    <!-- /.container -->
 
+              
 
 <!-- Bootstrap Core JavaScript -->
 	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
@@ -181,7 +192,7 @@
   $(document).ready(function() {
         var showChar = 300;
         var ellipsestext = "...";
-        var moretext = "leer m�s";
+        var moretext = "leer más";
         var lesstext = "ocultar";
         $('.more').each(function() {
             var content = $(this).html();
