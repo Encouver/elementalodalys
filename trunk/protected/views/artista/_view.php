7c15481df3b4f06bcd173fd61828c5041a355fdd
manturakabchi@gmail.com <manturakabchi@gmail.com@b283d022-b65f-89a2-4f4e-fbe6fd033296>
2014-09-09 04:44:48 +0000

    Generé el crud de Artista. Estaba probando gii

---
+++ trunk/protected/views/artista/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this ArtistaController */
+/* @var $data Artista */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('idartista')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->idartista), array('view', 'id'=>$data->idartista)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nombre')); ?>:</b>
+	<?php echo CHtml::encode($data->nombre); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('apellido')); ?>:</b>
+	<?php echo CHtml::encode($data->apellido); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('pais')); ?>:</b>
+	<?php echo CHtml::encode($data->pais); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('biografia')); ?>:</b>
+	<?php echo CHtml::encode($data->biografia); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('ano')); ?>:</b>
+	<?php echo CHtml::encode($data->ano); ?>
+	<br />
+
+
+</div>
