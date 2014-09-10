b727628a1e6d132d5abf871e5ea630ceffac69d2
edgar.leal0 <edgar.leal0@gmail.com>
2014-09-10 03:43:41 +0000

    Autenticacion y moificacion de controladores para el control de acceso.

--- protected/views/usuarios/admin.php
+++ protected/views/usuarios/admin.php
@@ -45,10 +45,10 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
 	'columns'=>array(
-		'id',
+		//'id',
 		'nombre',
 		'usuario',
-		'clave',
+		//'clave',
 		'fecha',
 		array(
 			'class'=>'CButtonColumn',
