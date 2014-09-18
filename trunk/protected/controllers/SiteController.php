b82d83eb11f3fe9c31581beaaa7afe737e3e763e
marcospha@gmail.com <marcospha@gmail.com@b283d022-b65f-89a2-4f4e-fbe6fd033296>
2014-09-18 11:27:29 +0000

    Configurado Smtpmail

--- trunk/protected/controllers/SiteController.php
+++ trunk/protected/controllers/SiteController.php
@@ -444,14 +444,14 @@ class SiteController extends Controller
 			if($model->validate())
 			{
 				$name='=?UTF-8?B?'.base64_encode($model->name).'?=';
-				$subject='=?UTF-8?B?'.'ELEMENTAL '.base64_encode($model->subject).'?=';
+				$subject='=?UTF-8?B?'.base64_encode($model->subject).'?=';
 				$headers="From: $name <{$model->email}>\r\n".
 					"Reply-To: {$model->email}\r\n".
 					"MIME-Version: 1.0\r\n".
 					"Content-Type: text/plain; charset=UTF-8";
 
 				mail(Yii::app()->params['adminEmail'],$subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact',Yii::t('site','Gracias por contactarnos. Le daremos respuesta a la brevedad posible.'));
+				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
 				$this->refresh();
 			}
 		}
