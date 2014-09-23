e21c1408d175089124bcd39751a0055d7386f136
manturakabchi@gmail.com <manturakabchi@gmail.com@b283d022-b65f-89a2-4f4e-fbe6fd033296>
2014-09-23 02:48:08 +0000

    Suscríbete no funcional aún

--- trunk/protected/views/site/index.php
+++ trunk/protected/views/site/index.php
@@ -159,6 +159,24 @@ echo "<hr>";
 	</div>
 
 	<div class="col-md-3 col-md-offset-1">
+
+
+	    <h4 style="margin-top:0px">¡Suscríbete!</h4>
+	    <p style ="font-size: 12px;">
+	    Recibe información de ELEMENTAL Proyecto en tu correo electrónico
+	    <form class="form-horizontal" role="form">
+			<input type="text" class="form-control input-sm" placeholder="mail@domain.com">
+			
+			<button type="button" class="btn btn-sm" style="width:164px">
+			  <span class="glyphicon glyphicon-send"></span> Suscribirse
+			</button>
+		</form>
+
+	    </p>
+
+	    <br>
+	    <br>
+
 	    <h4 style="margin-top:0px">ELEMENTAL Proyecto</h4>
 	    <p style ="font-size: 13px;">
 	<?php
